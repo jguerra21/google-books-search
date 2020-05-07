b0f786dc8e04302ef1752a78f0cf60701ddb6bc1
Jolie Guerra <Jolie_guerra@yahoo.com>
2020-05-07 15:28:32 -0400

    commit

---
+++ client/src/utils/API.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export default {
+  // Google books search
+  search: function(query) {
+    return axios.get(
+      "https://www.googleapis.com/books/v1/volumes?q=" + query + "&maxResults=5"
+    );
+  },
+  // Get favorites
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Save book
+  saveBook: function(data) {
+    return axios.post("/api/books", data);
+  },
+  // Delete book
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+};
